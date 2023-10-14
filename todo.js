20591efe7a4e80320e6c7511d46f5ff89ae06dc3
kenblocklive <140394938+kenblocklive@users.noreply.github.com>
2023-10-14 17:09:41 +0400

    Feat/checkbox (#1)
    * init checkbox branch
    
    * add input checkbox

--- todo.js
+++ todo.js
@@ -5,21 +5,17 @@ const listItems = list.getElementsByTagName("li");
 
 button.addEventListener("click", () => {
   const listItem = document.createElement("li");
+  const checkBox = document.createElement("input");
+  checkBox.setAttribute("type", "checkbox");
   listItem.textContent = input.value;
+  listItem.prepend(checkBox);
   list.appendChild(listItem);
-  newEvent(listItem);
+  newEvent(listItem, checkBox);
   input.value = null;
 });
 
-for (let i = 0; i < listItems.length; i++) {
-  newEvent(listItems[i]);
-}
-function newEvent(listItem) {
-  listItem.addEventListener("click", () => {
-    if (listItem.className === "completed") {
-      listItem.removeAttribute("class");
-    } else {
-      listItem.setAttribute("class", "completed");
-    }
+function newEvent(listItem, checkBox) {
+  checkBox.addEventListener("click", () => {
+    listItem.classList.toggle("completed");
   });
 }
