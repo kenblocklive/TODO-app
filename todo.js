1db01131ac0de849660266ff75ca2f06683a9c1a
kenblocklive <kenblock@live.ru>
2023-10-15 17:28:11 +0400

    feat/ delete task Co-authored-by: Dmitry Kungurov <kuraiconnect@gmail.com>

--- todo.js
+++ todo.js
@@ -1,21 +1,49 @@
 const input = document.getElementsByTagName("input")[0];
 const button = document.getElementsByTagName("button")[0];
-const list = document.getElementsByTagName("ul")[0];
-const listItems = list.getElementsByTagName("li");
+const list = document.getElementsByTagName("ul")[0]; // <ul></ul>
 
-button.addEventListener("click", () => {
-  const listItem = document.createElement("li");
-  const checkBox = document.createElement("input");
-  checkBox.setAttribute("type", "checkbox");
-  listItem.textContent = input.value;
-  listItem.prepend(checkBox);
-  list.appendChild(listItem);
-  newEvent(listItem, checkBox);
+button.addEventListener("click", () => addTask());
+
+function addTask() {
+  const id = (Math.random() * 100).toFixed(0);
+  const listItem = `
+  <li data-id="${id}">
+    <input type="checkbox"/>
+    <span>${input.value}</span>
+    <button onclick="deleteTask(${id})">Delete</button>
+  </li>`;
+
+  list.insertAdjacentHTML("afterBegin", listItem);
+  const items = list.getElementsByTagName("li");
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    const checkbox = item.getElementsByTagName("input")[0];
+    const textTask = item.getElementsByTagName("span")[0];
+    checkbox.addEventListener("input", () => toggleTask(textTask));
+  }
+
   input.value = null;
-});
+}
 
-function newEvent(listItem, checkBox) {
-  checkBox.addEventListener("click", () => {
-    listItem.classList.toggle("completed");
-  });
+function toggleTask(item) {
+  item.classList.toggle("completed");
+}
+
+{
+  /* <li data-id="12">
+    <input type="checkbox"/>
+    <span>${input.value}</span>
+    <button onclick="deleteTask(12)">Delete</button>
+  </li> */
+}
+function deleteTask(taskId) {
+  const items = list.getElementsByTagName("li");
+  for (let i = 0; i < items.length; i++) {
+    const item = items[i];
+    const id = item.getAttribute("data-id");
+    if (+id === taskId) {
+      list.removeChild(item);
+      return;
+    }
+  }
 }
